a6a455ba830f51656d64b1701f082d7b0dad3597
Arthur Fernandes <arthurfernandes1666@gmail.com>
2024-11-13 19:19:30 -0300

    feat: menu

--- app/src/main/java/com/example/trabson/MainActivity.java
+++ app/src/main/java/com/example/trabson/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.trabson;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -11,6 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar tbMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding();
+        setSupportActionBar(tbMain);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.mnAcoes) {
+            Toast.makeText(getApplicationContext(), "Clicou em ações!", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.mnCarteira) {
+            Toast.makeText(getApplicationContext(), "Clicou em carteira!", Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void binding() {
+        tbMain = findViewById(R.id.tbMain);
     }
 }
